fd68e5516bd32ac8d45773f69e51459bf06de3a6
Yeseo Kim <YKim@lcs.on.ca>
2022-01-20 12:05:08 -0500

    Added computed property to show different colours depending on the task priority

--- Shared/Views/TaskCell.swift
+++ Shared/Views/TaskCell.swift
@@ -11,6 +11,17 @@ struct TaskCell: View {
     
     @ObservedObject var task: Task
     
+    var taskColor: Color {
+        switch task.priority {
+        case .high:
+            return Color.red
+        case .medium:
+            return Color.blue
+        case .low:
+            return Color.primary
+        }
+    }
+    
     var body: some View {
         HStack {
             Image(systemName: task.completed ? "checkmark.cirlce.fill" : "circle")
@@ -21,6 +32,7 @@ struct TaskCell: View {
                 }
             Text(task.description)
         }
+        .foregroundColor(self.taskColor)
     }
 }
 
