b0abae1f65193ab599a410b5334d2f8d432ae116
Yeseo Kim <YKim@lcs.on.ca>
2022-01-20 11:18:29 -0500

    Initial Commit

---
+++ Shared/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Shared
+//
+//  Created by Yeseo Kim on 2022-01-20.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
